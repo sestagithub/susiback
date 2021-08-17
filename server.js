39a93aa9c2f73e795f1a86dd91056622765eb636
SeSTA <info@sesta.org>
2021-08-17 16:33:59 +0530

    'g'

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const mysql = require('mysql');
 const bodyparser = require ("body-parser");
 var cors = require('cors');
 const app = express();
-var db = require('./db');
+// var db = require('./db');
  
-app.use(cors());
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:false}));
+// app.use(cors());
+// app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({extended:false}));
 
 
 
 
-app.use('/api/users', require('./routes/user'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/baseline', require('./routes/baseline'));
-app.use('/api/midline',require('./routes/midline'));
-app.use('/api/data',require('./routes/data'));
-app.use('/api/master',require('./routes/master'));
+// app.use('/api/users', require('./routes/user'));
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/baseline', require('./routes/baseline'));
+// app.use('/api/midline',require('./routes/midline'));
+// app.use('/api/data',require('./routes/data'));
+// app.use('/api/master',require('./routes/master'));
 
 
 app.get('/',(req,res)=>{
